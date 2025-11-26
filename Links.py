0cdc1b38a533a532d1864c269367cbf14185e033
Mohamed Elsharkawy <moelsharkawy03@gmail.com>
2025-11-26 19:46:59 +0200

    Update Links.py

--- Links.py
+++ Links.py
@@ -1,105 +1,118 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 25 10:42:10 2021
+import numpy as np
+from scipy.optimize import fsolve
 
-@author: Martín Tobar
-"""
-import math
-import matplotlib.pyplot as plt
-def coseno(angulo):
-    return math.cos(math.radians(angulo))
+class FourBarMechanism:
+    def __init__(self, a, b, c, d):
+        """
+        Initialize with link lengths.
+        a: crank length
+        b: coupler length
+        c: rocker length
+        d: ground length
+        """
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
 
-#Initialize arrays that hold constants for each angle.
-A = [0.0]*360
-B = [0.0]*360
-C = [0.0]*360
-D = [0.0]*360
-E = [0.0]*360
-F = [0.0]*360
-A4O = [0.0]*360
-A4C = [0.0]*360
-A3O = [0.0]*360
-A3C = [0.0]*360
+    def position_analysis(self, theta2):
+        """
+        Solve for theta3 and theta4 given theta2.
+        Returns: theta3, theta4
+        """
+        def equations(vars):
+            theta3, theta4 = vars
+            eq1 = self.a * np.cos(theta2) + self.b * np.cos(theta3) - self.c * np.cos(theta4) - self.d
+            eq2 = self.a * np.sin(theta2) + self.b * np.sin(theta3) - self.c * np.sin(theta4)
+            return [eq1, eq2]
+        
+        # Initial guess: slightly offset from theta2
+        initial_guess = [theta2 + 0.1, theta2 + 0.1]
+        sol = fsolve(equations, initial_guess)
+        theta3, theta4 = sol
+        return theta3, theta4
 
-#Length of the links in meters
-a = 0.04
-b = 0.120
-c = 0.08 
-d = 0.1
+    def velocity_analysis(self, theta2, omega2, theta3, theta4):
+        """
+        Solve for omega3 and omega4 given theta2, omega2, and positions.
+        Returns: omega3, omega4
+        """
+        # Coefficients from differentiated loop equations
+        A11 = -self.b * np.sin(theta3)
+        A12 = self.c * np.sin(theta4)
+        A21 = self.b * np.cos(theta3)
+        A22 = -self.c * np.cos(theta4)
+        
+        B1 = -self.a * omega2 * np.sin(theta2)
+        B2 = self.a * omega2 * np.cos(theta2)
+        
+        # Matrix form: A * [omega3, omega4]^T = B
+        A = np.array([[A11, A12], [A21, A22]])
+        B = np.array([B1, B2])
+        
+        omega3, omega4 = np.linalg.solve(A, B)
+        return omega3, omega4
 
-#Constants calculation 
-k1 = d/a
-k2 = d/c
-k3 = (pow(a,2)-pow(b,2)+pow(c,2)+pow(d,2))/(2*a*c)
+    def acceleration_analysis(self, theta2, omega2, alpha2, theta3, theta4, omega3, omega4):
+        """
+        Solve for alpha3 and alpha4 given theta2, omega2, alpha2, positions, and velocities.
+        Returns: alpha3, alpha4
+        """
+        # Second derivatives of loop equations
+        A11 = -self.b * np.sin(theta3)
+        A12 = self.c * np.sin(theta4)
+        A21 = self.b * np.cos(theta3)
+        A22 = -self.c * np.cos(theta4)
+        
+        # Right-hand side includes accelerations and velocity terms
+        B1 = -self.a * (alpha2 * np.sin(theta2) + omega2**2 * np.cos(theta2)) + \
+             self.b * omega3**2 * np.cos(theta3) - self.c * omega4**2 * np.cos(theta4)
+        B2 = self.a * (alpha2 * np.cos(theta2) - omega2**2 * np.sin(theta2)) - \
+             self.b * omega3**2 * np.sin(theta3) + self.c * omega4**2 * np.sin(theta4)
+        
+        A = np.array([[A11, A12], [A21, A22]])
+        B = np.array([B1, B2])
+        
+        alpha3, alpha4 = np.linalg.solve(A, B)
+        return alpha3, alpha4
 
-k4 = d/b
-k5 = (-pow(a,2)-pow(b,2)+pow(c,2)-pow(d,2))/(2*a*b)
+    def analyze(self, theta2, omega2=1.0, alpha2=0.0):
+        """
+        Full analysis: position, velocity, acceleration.
+        Returns: dict with theta3, theta4, omega3, omega4, alpha3, alpha4
+        """
+        theta3, theta4 = self.position_analysis(theta2)
+        omega3, omega4 = self.velocity_analysis(theta2, omega2, theta3, theta4)
+        alpha3, alpha4 = self.acceleration_analysis(theta2, omega2, alpha2, theta3, theta4, omega3, omega4)
+        
+        return {
+            'theta3': theta3,
+            'theta4': theta4,
+            'omega3': omega3,
+            'omega4': omega4,
+            'alpha3': alpha3,
+            'alpha4': alpha4
+        }
 
-#print(k1,k2,k3,k4,k5)
-
-#Obtain each constant and auxiliar value for each angle.
-for angle in range (360):
-    A[angle] = coseno(angle) -k1 - k2*coseno(angle)+k3
-    B[angle] = -2*math.sin(math.radians(angle))
-    C[angle] = k1- (k2+1)*coseno(angle) + k3 
-    #Open theta4
-    A4O[angle] = math.degrees(2*math.atan( (-B[angle]-math.sqrt(pow(B[angle],2)-4*A[angle]*C[angle]))/(2*A[angle])   ))
-    #Crossed theta 4
-    A4C[angle] = math.degrees(2*math.atan( (-B[angle]+math.sqrt(pow(B[angle],2)-4*A[angle]*C[angle]))/(2*A[angle])   ))
+# Example usage
+if __name__ == "__main__":
+    # Define a four-bar mechanism (example lengths)
+    mechanism = FourBarMechanism(a=1.0, b=2.0, c=1.5, d=2.5)
     
+    # Input: crank angle (radians), angular velocity, angular acceleration
+    theta2 = np.pi / 4  # 45 degrees
+    omega2 = 1.0  # rad/s
+    alpha2 = 0.0  # rad/s²
     
-    D[angle] = coseno(angle) - k1 + k4*coseno(angle) + k5
-    E[angle] = -2*math.sin(math.radians(angle))
-    F[angle] = k1 + (k4-1)*coseno(angle) +k5
-     #Open theta3
-    A3O[angle] = math.degrees(2*math.atan( (-E[angle]-math.sqrt(pow(E[angle],2)-4*D[angle]*F[angle]))/(2*D[angle])   ))
-     #Crossed theta3
-    A3C[angle] = math.degrees(2*math.atan( (-E[angle]+math.sqrt(pow(E[angle],2)-4*D[angle]*F[angle]))/(2*D[angle])   ))
-
-
-
-#animation
-
-for m_angle in range (0,360,1):
-
-    #For each angle plot each link depending on the position of the angle m_angle which is theta 2.
-    L1X = [0,d*1000]
-    L1Y = [0,0]
+    # Perform analysis
+    results = mechanism.analyze(theta2, omega2, alpha2)
     
-    L2X = [L1X[0],L1X[0]+math.cos(math.radians(m_angle))*a*1000]
-    L2Y = [L1Y[0],L1Y[0]+math.sin(math.radians(m_angle))*a*1000]
-    
-    L3X = [L2X[1],L2X[1]+math.cos(math.radians(A3O[m_angle])) *b*1000]
-    L3Y = [L2Y[1],L2Y[1]+math.sin(math.radians(A3O[m_angle])) *b*1000]
-    
-    L4X = [L1X[1],L1X[1]-math.cos(math.radians(180-A4O[m_angle])) *c*1000]
-    L4Y = [L1Y[1],L1Y[1]+math.sin(math.radians(180-A4O[m_angle])) *c*1000]
-    
-   
-    plt.title("Movement in function of $\\theta 2$")
-    
-    #Plot lines aka LINKS
-    plt.plot(L1X,L1Y,label =("L1 "+str(d*1000)+" mm"))
-    plt.plot(L2X,L2Y,label =("L2 "+str(a*1000)+" mm"))
-    plt.plot(L3X,L3Y,label =("L3 "+str(b*1000)+" mm"))
-    plt.plot(L4X,L4Y,label =("L4 "+str(c*1000)+" mm"))
-    
-   
-   #Plot angles values
-    plt.text(L2X[1]/10,1,"$\\theta_2 = $"+str(m_angle))
-    plt.text(L2X[1]+5,L2Y[1]+1,"$\\theta_3 = $"+str(format(A3O[m_angle],".3f")))
-    plt.text(L1X[1]+1,L1Y[1]+5,"$\\theta_4 = $"+str(format(A4O[m_angle],".3f")))
-    
-    #Plot reference lines for angles theta 3 and 4
-    plt.plot([L2X[1],L2X[1]+10],[L2Y[1],L2Y[1]],color="black")
-    plt.plot([L1X[1],L1X[1]+20],[0,0],color="black")
-  
-    
-    #Add a legend to see which color is each link.
-    leg = plt.legend(loc='upper left')
-    #Add a grid
-    plt.grid()
-    #Save the plot on animation folder with the name of the theta2, dpi is for resoultion
-    plt.savefig('animation/'+str(m_angle),dpi=300)
-    #Show the plot
-    plt.show()
+    # Print results
+    print("Four-Bar Mechanism Analysis:")
+    print(f"Input: θ₂ = {theta2:.3f} rad, ω₂ = {omega2} rad/s, α₂ = {alpha2} rad/s²")
+    print(f"θ₃ = {results['theta3']:.3f} rad")
+    print(f"θ₄ = {results['theta4']:.3f} rad")
+    print(f"ω₃ = {results['omega3']:.3f} rad/s")
+    print(f"ω₄ = {results['omega4']:.3f} rad/s")
+    print(f"α₃ = {results['alpha3']:.3f} rad/s²")
+    print(f"α₄ = {results['alpha4']:.3f} rad/s²")
